5610f7ef2fabb84ac561d8582aad3997b89846b9
Thom Veldhuis <thomveldhuis03@gmail.com>
2021-04-16 14:22:26 +0200

    book class is finished

--- boeken/models/Book.php
+++ boeken/models/Book.php
@@ -6,6 +6,41 @@
     public $title = "";
     public $author = "";
     public $isbn = "";
+
+    public function __construct($id = NULL)
+    {
+      //is er een id mee gegeven?
+      if (!is_null($id))
+      {
+        $this->load($id);
+      }
+    }
+
+    private function load($id)
+    {
+      global $mysql;
+
+      //query om het boek te vinden
+      $sql = "SELECT * FROM `mvc_boeken` WHERE id = {$id}";
+
+      $result = mysqli_query($mysql, $sql);
+
+      if (mysqli_num_rows($result) == 1)
+      {
+        //lees de gegevens uit
+        $row = mysqli_fetch_array($result);
+
+        //vul de properties in met de data
+        $this->id = $id;
+        $this->title = $row['title'];
+        $this->author = $row['author'];;
+        $this->isbn = $row['isbn'];;
+      } else
+      {
+        //er gaat wat mis
+        throw new Exception("Kan het boek met ID ". $id ." niet vinden!");
+      }
+    }
   }
 
  ?>
