ff6ceba94698fd373dde0555ce81757f971a3563
Thom Veldhuis <thomveldhuis03@gmail.com>
2021-04-16 14:34:17 +0200

    tested books class out with making one and getting one out the db

--- boeken/index.php
+++ boeken/index.php
@@ -1,4 +1,18 @@
 <?php
   require "../includes/config.php";
+  //get the book class
+  require "models/Book.php";
 
+  //is zelf gemaakte boek
+  $b = new Book();
+  $b->title = "1984";
+  $b->author = "George Orwell";
+  $b->isbn = "9780141036144";
+
+  echo $b->title." is de Titel <br>".$b->author." is de Author <br>".$b->isbn." is de isbn <br>";
+
+  //is een boek al uit de database
+  $b2 = new Book(2);
+
+  echo $b2->title." is de Titel <br>".$b2->author." is de Author <br>".$b2->isbn." is de isbn <br>";
  ?>
