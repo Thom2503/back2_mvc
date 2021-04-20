6b07b0103d04e7200cb0307b8548e6495682361a
Thom Veldhuis <thomveldhuis03@gmail.com>
2021-04-20 10:09:03 +0200

    added save and update to book

--- boeken/index.php
+++ boeken/index.php
@@ -15,4 +15,5 @@
   $b2 = new Book(2);
 
   echo $b2->title." is de Titel <br>".$b2->author." is de Author <br>".$b2->isbn." is de isbn <br>";
+  $b->save();
  ?>
--- boeken/models/Book.php
+++ boeken/models/Book.php
@@ -33,12 +33,52 @@
         //vul de properties in met de data
         $this->id = $id;
         $this->title = $row['title'];
-        $this->author = $row['author'];;
-        $this->isbn = $row['isbn'];;
+        $this->author = $row['author'];
+        $this->isbn = $row['isbn'];
       } else
       {
         //er gaat wat mis
         throw new Exception("Kan het boek met ID ". $id ." niet vinden!");
+      }
+    }
+
+    public function save()
+    {
+      global $mysql;
+
+      //maak de tekst schoon
+      $this->title = htmlentities(html_entity_decode($this->title, ENT_QUOTES), ENT_QUOTES);
+      $this->author = htmlentities(html_entity_decode($this->author, ENT_QUOTES), ENT_QUOTES);
+      $this->isbn = htmlentities(html_entity_decode($this->isbn, ENT_QUOTES), ENT_QUOTES);
+
+      if (strlen($this->title) > 0 && strlen($this->author) > 0 && strlen($this->isbn) > 0)
+      {
+        //controleer of dit een nieuw of bestaand boek is
+        if (is_null($this->id))
+        {
+          $sql = "INSERT INTO `mvc_boeken`(`title`, `author`, `isbn`)
+          VALUES ('$this->title','$this->author','$this->isbn')";
+
+          $result = mysqli_query($mysql, $sql);
+
+          if (!$result)
+          {
+            //er gaat wat mis
+            throw new Exception("Er is iets mis gegaan probeer het later opnieuw!");
+          }
+        } else
+        {
+          $sql = "UPDATE `mvc_boeken` SET
+          `title`='$this->title',`author`='$this->author',`isbn`=$this->isbn WHERE `id` = $this->id";
+
+          $result = mysqli_query($mysql, $sql);
+
+          if (!$result)
+          {
+            //er gaat wat mis
+            throw new Exception("Er is iets mis gegaan probeer het later opnieuw!");
+          }
+        }
       }
     }
   }
